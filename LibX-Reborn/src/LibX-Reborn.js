1085b6dda0c771f990ccf749b0cc7578bf06a9cf
Sanooj Es <sanoojes6371@gmail.com>
2024-09-12 01:56:21 +0530

    :sparkles: feat: Release `LibX-Reborn` Initial Commit

---
+++ LibX-Reborn/src/LibX-Reborn.js
@@ -0,0 +1,299 @@
+const GLOBAL_NAV_SELECTOR = '.Root__globalNav';
+
+const checkForGlobalNav = () =>
+  document.querySelector('.globalNav') ||
+  document.querySelector('.Root__globalNav') ||
+  false;
+
+let isGlobalNavAvailable = checkForGlobalNav();
+
+const customStyles = `
+.Root__top-container {
+padding-top: 64px !important;
+grid-template-areas:
+  "global-nav main-view right-sidebar"
+  "left-sidebar main-view right-sidebar"
+  "now-playing-bar now-playing-bar now-playing-bar" !important;
+
+grid-template-rows: auto 1fr auto;
+}
+
+.Root__globalNav>* {
+padding-left: 12px;
+padding-right: 12px;
+}
+
+.Root__globalNav {
+height: unset;
+background-color: var(--spice-main);
+border-radius: 8px;
+padding-inline: unset !important;
+align-items: unset;
+padding: 8px;
+}
+
+.Root__globalNav .main-globalNav-link-icon {
+background-color: unset;
+transform: unset;
+justify-content: unset;
+}
+
+.main-actionButtons {
+display: inline-flex !important;
+animation: unset !important;
+}
+
+.main-globalNav-historyButtons>* {
+display: unset;
+}
+
+
+.main-globalNav-historyButtons {
+order: 1;
+display: unset;
+position: absolute;
+bottom: 8px;
+right: 8px;
+}
+
+.main-globalNav-historyButtonsContainer {
+display: grid;
+order: 0;
+}
+
+.main-globalNav-searchSection {
+order: -1;
+}
+
+.main-globalNav-searchInputSection {
+margin-inline: unset;
+}
+
+.Root__globalNav .Dp3xccI7c2f_JSJ8OHYu {
+margin-inline-start: unset;
+}
+
+.Root__globalNav,
+.main-globalNav-searchContainer,
+.aKOZdeebnsaeeMTiugmO {
+flex-direction: column !important;
+}
+
+.SFAoASy0S_LZJmYZ3Fh9 {
+background: unset;
+background-color: unset !important;
+border-bottom: unset;
+}
+
+.main-topBar-topbarContentRight {
+justify-content: unset;
+}
+
+.main-globalNav-iconText:not(.main-globalNav-link-icon) {
+font-size: 1rem;
+font-weight: 700;
+margin-inline-start: 12px;
+}
+
+.main-globalNav-historyButtons,
+.Root__globalNav .main-topBar-topbarContentRight {
+position: fixed !important;
+top: 0;
+padding: var(--panel-gap);
+height: max-content;
+width: max-content;
+}
+
+.main-globalNav-historyButtons {
+left: 4vw;
+}
+
+.Root__globalNav .main-topBar-topbarContentRight {
+right: 10vw;
+}
+
+.main-globalNav-navLinkActive {
+background-color: var(--spice-card) !important;
+border-radius: 0.5rem !important;
+}
+
+.Root__globalNav {
+--card-gap: 0.25rem;
+}
+
+.custom-navlinks-scrollable_container {
+-webkit-app-region: no-drag;
+max-width: 100% !important;
+width: 100% !important;
+}
+
+.Root__globalNav .main-globalNav-searchContainer,
+.Root__globalNav .search-searchCategory-categoryGrid>div[role="presentation"] {
+display: flex;
+flex-direction: column !important;
+width: 100% !important;
+row-gap: var(--card-gap);
+}
+
+.Root__globalNav .search-searchCategory-categoryGrid>div[role="presentation"]>button {
+/* background-color: var(--spice-card); */
+border-radius: 0.5rem;
+}
+
+
+.custom-navlinks-scrollable_container {
+margin-top: var(--card-gap);
+}
+
+.custom-navlinks-scrollable_container div[role="presentation"]>* {
+margin: 0 !important;
+}
+
+.Root__globalNav .main-globalNav-searchInputSection {
+display: flex;
+align-items: center;
+}
+
+.forceExpandSearchInput .main-globalNav-searchInputSection .main-globalNav-textWrapper {
+display: none;
+}
+
+.main-globalNav-textWrapper {
+margin-left: 1rem;
+}
+
+.main-globalNav-searchInputSection .main-globalNav-textWrapper {
+position: absolute;
+margin-left: 3.25rem;
+top: 50%;
+transform: translateY(-50%);
+}
+
+.main-globalNav-searchText {
+font-size: 1rem;
+font-weight: 700;
+flex-direction: row;
+display: flex;
+align-items: center;
+}
+
+.main-globalNav-searchInputSection,
+.searchInputCollapsed:not(.forceExpandSearchInput) .main-globalNav-searchInputSection>.main-globalNav-searchInputContainer {
+width: 100% !important;
+}
+
+
+.main-globalNav-searchInputContainer .SFAoASy0S_LZJmYZ3Fh9:hover,
+.main-globalNav-searchInputContainer input:hover {
+background-color: var(--spice-card) !important;
+}
+
+.searchInputCollapsed .main-globalNav-searchInputContainer .jl5Sca1FSi1bSBIyQ72h {
+visibility: hidden;
+}
+
+.forceExpandSearchInput .main-globalNav-searchInputContainer .jl5Sca1FSi1bSBIyQ72h,
+.searchInputCollapsed.forceExpandSearchInput .main-globalNav-searchInputContainer .jl5Sca1FSi1bSBIyQ72h {
+visibility: visible !important;
+}
+
+:root {
+/* Set sidebar width as 360px for now */
+--left-sidebar-width: 360 !important;
+}
+`;
+
+const addGlobalNavStyles = () => {
+  const elements = document.querySelectorAll(
+    '.search-searchCategory-categoryGrid > div > button, .main-globalNav-searchContainer > .main-globalNav-link-icon, .main-globalNav-searchInputSection'
+  );
+
+  for (const element of elements) {
+    const isSearchElement = element.querySelector('input');
+    const textElement = element.querySelector(
+      '.main-globalNav-searchText.encore-text.encore-text-body-medium-bold'
+    );
+
+    if (!textElement) {
+      const newTextElement = document.createElement('div');
+      newTextElement.className =
+        'main-globalNav-searchText encore-text encore-text-body-medium-bold';
+      newTextElement.textContent =
+        element.getAttribute('aria-label') ||
+        (isSearchElement ? 'Search' : '') ||
+        '';
+      const newTextWrapperElement = document.createElement('span');
+      newTextWrapperElement.className = 'main-globalNav-textWrapper';
+      newTextWrapperElement.appendChild(newTextElement);
+
+      if (isSearchElement) {
+        const iconElement = element.querySelector(
+          '.main-globalNav-searchInputContainer>:first-child'
+        );
+        iconElement
+          ? iconElement.appendChild(newTextWrapperElement)
+          : element.appendChild(newTextWrapperElement);
+      } else {
+        element.appendChild(newTextWrapperElement);
+      }
+    }
+  }
+};
+
+(async () => {
+  try {
+    console.log('Running GlobalNav to LibraryX script...');
+
+    let attempts = 0;
+    const maxAttempts = 3;
+
+    const checkGlobalNav = async () => {
+      isGlobalNavAvailable = checkForGlobalNav();
+      attempts++;
+
+      if (isGlobalNavAvailable) {
+        // GlobalNav found, proceed with script execution
+        const styleSheetId = 'libraryX-styles';
+
+        let styleElement = document.getElementById(`#${styleSheetId}`);
+        if (!styleElement) {
+          styleElement = document.createElement('style');
+          styleElement.id = styleSheetId;
+          styleElement.innerHTML = customStyles;
+          document.head.appendChild(styleElement);
+        } else {
+          styleElement.innerHTML = customStyles;
+        }
+
+        // Initial toggle
+        addGlobalNavStyles();
+
+        // Wait for 200ms then start observing changes
+        setTimeout(() => {
+          const observer = new MutationObserver(addGlobalNavStyles);
+          const globalNavElement = document.querySelector(GLOBAL_NAV_SELECTOR);
+          if (globalNavElement) {
+            observer.observe(globalNavElement, {
+              childList: true,
+              subtree: true,
+            });
+          }
+        }, 200);
+      } else if (attempts < maxAttempts) {
+        // GlobalNav not found, wait and try again
+        console.log('GlobalNav not found, retrying...');
+        await new Promise((resolve) => setTimeout(resolve, 1000));
+        checkGlobalNav();
+      } else {
+        console.error(
+          'GlobalNav to Library Script is not available on this nav mode.'
+        );
+      }
+    };
+
+    await checkGlobalNav();
+  } catch (error) {
+    console.error('Error running GlobalNav to LibraryX script:', error);
+  }
+})();
+
---
+++ readme.md
@@ -0,0 +1,42 @@
+# Spicetify Extensions
+
+This repository contains a Spicetify extension created by me. [Sanooj Es](github.com/sanoojes)
+
+## LibX Reborn
+
+### **Description:**
+
+Enjoy a familiar and convenient way to manage your music library with LibX Reborn! This extension seamlessly re-integrates the classic Library X view into the new Global Nav interface, giving you a consistent and user-friendly experience.
+
+### **Features:**
+
+- **Restores Library X:** Experience the classic Library X layout, offering a convenient way to browse and manage your music library.
+
+### **Screenshots:**
+
+![LibX Reborn Screenshot](./LibX-Reborn/assets/sc1.png)
+![LibX Reborn Screenshot](./LibX-Reborn/assets/sc2.png)
+
+### **Installation:**
+
+1. **Download:** Download the latest version of the LibX Reborn extension: [Download LibX Reborn](https://github.com/sanoojes/spicetify-extenstions/raw/main/LibX-Reborn/src/LibX-Reborn.js)
+2. **Locate Spicetify Config Folder:** Open your Spicetify Config folder, typically located at:
+   - **macOS/Linux:** `~/.config/spicetify/`
+   - **Windows:** `%AppData%\Roaming\spicetify`
+3. **Place Extension File:** Navigate to the "Extensions" folder within the Spicetify Config folder and place the downloaded `LibX-Reborn.js` file inside.
+4. **Install:** Open your terminal (or Command Prompt on Windows) and run the command: `spicetify config extensions LibX-Reborn.js`
+5. **Apply Changes:** Run the command: `spicetify apply`
+6. **Restart Spotify:** Restart Spotify for the changes to take effect.
+
+### **Usage:**
+
+Once installed, the Library X view will become available again within the Spotify interface. You'll find it where your music library options are usually located.
+
+### **Known Issues:**
+
+- The extension may not be fully compatible with all themes or modifications to the Global Nav.
+- If you encounter any issues, please submit a bug report on this repository.
+
+### **Contribute:**
+
+Contributions are welcome! Feel free to submit bug reports, feature requests, or even code improvements.
