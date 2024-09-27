3dad0dcc963f1466f0d367c03abb13bbb80afaaa
Sanooj Es <sanoojes6371@gmail.com>
2024-09-27 19:37:39 +0530

    :sparkles: feat: add remote script for libX

---
+++ LibX-Reborn/remote/LibX-Reborn.js
@@ -0,0 +1,7 @@
+(() => {
+  const themeScript = document.createElement('script');
+  themeScript.type = 'text/javascript';
+  themeScript.src =
+    'https://sanooj.is-a.dev/spicetify-extensions/LibX-Reborn/src/LibX-Reborn.js';
+  document.head.appendChild(themeScript);
+})();
